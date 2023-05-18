b2f87229cee01b9c21148d1239d62f52f083e28a
DentistLeeroy <74152951+DentistLeeroy@users.noreply.github.com>
2023-05-18 13:14:33 +0200

    Navigation added to all pages

--- book-swap/pages/history.tsx
+++ book-swap/pages/history.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { useRouter } from 'next/router';
+import { Avatar, Box, ChakraProvider, Flex, Link, VStack, Text } from '@chakra-ui/react';
 
 type BottomNavItem = {
   label: string;
@@ -13,7 +14,7 @@ const bottomNavItems: BottomNavItem[] = [
   { label: 'Profile', path: '/profile' },
 ];
 
-const ProfilePage: React.FC = () => {
+const HistoryPage: React.FC = () => {
   const router = useRouter();
   const { pathname } = router;
 
@@ -22,33 +23,45 @@ const ProfilePage: React.FC = () => {
   };
 
   return (
-    <div>
-      {/* Render your profile page content here */}
-
-      {/* Render bottom navigation */}
-      <nav>
-        <ul>
+    <ChakraProvider>
+      <Flex height="100vh" width="100vw">
+        <VStack align="flex-start" spacing={4} pr={8} borderRight="1px solid" borderColor="gray.200">
+          {/* Render bottom navigation */}
           {bottomNavItems.map((item) => (
-              <li
+            <Link
               key={item.path}
-              className={pathname === item.path ? 'active' : ''}
               onClick={() => handleNavItemClicked(item.path)}
-              >
+              color={pathname === item.path ? 'blue.500' : 'gray.500'}
+              fontWeight={pathname === item.path ? 'bold' : 'normal'}
+              p={2}
+            >
               {item.label}
-            </li>
+            </Link>
           ))}
-        </ul>
-      </nav>
+        </VStack>
 
-    <h1>History</h1>
-      {/* Add styles for the active bottom navigation item */}
-      <style jsx>{`
-        .active {
-          font-weight: bold;
-        }
-      `}</style>
-    </div>
+        {/* Render your history page content here */}
+        <Flex flex={1} justifyContent="center" alignItems="center" bg="gray.100">
+          <Box p={4}>
+            <VStack spacing={4} mt={4} maxWidth={400}>
+              <Flex alignItems="center">
+                <Avatar name="John Doe" size="sm" />
+                <Text ml={2}>History item 1</Text>
+              </Flex>
+              <Flex alignItems="center">
+                <Avatar name="Jane Smith" size="sm" />
+                <Text ml={2}>History item 2</Text>
+              </Flex>
+              <Flex alignItems="center">
+                <Avatar name="John Doe" size="sm" />
+                <Text ml={2}>History item 3</Text>
+              </Flex>
+            </VStack>
+          </Box>
+        </Flex>
+      </Flex>
+    </ChakraProvider>
   );
 };
 
-export default ProfilePage;
+export default HistoryPage;
